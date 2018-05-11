e83c1c20374ba5cb9aed28b79935acb34e268e19
Alexander Schwartzberg <aeksco@gmail.com>
2018-05-10 23:39:15 -0400

    ng new my-app - neat.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -13,4 +12,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
